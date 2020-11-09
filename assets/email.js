98a33f86bf1963696f9fa60c31efb28f77aaaf5d
Ryan Zelazny <RyanZelazny@gmail.com>
2020-11-09 13:44:50 -0500

    script link added

--- assets/email.js
+++ assets/email.js
@@ -1,16 +1,16 @@
 //var API = configVariables.API;
 
 // Node.js SDK: https://github.com/sendinblue/APIv3-nodejs-library
-import { ApiClient, ContactsApi, CreateContact } from 'sib-api-v3-sdk';
-var defaultClient = ApiClient.instance;
+var SibApiV3Sdk = require('sib-api-v3-sdk');
+var defaultClient = SibApiV3Sdk.ApiClient.instance;
 
 // Configure API key authorization: api-key
 var apiKey = defaultClient.authentications['api-key'];
 apiKey.apiKey = configVariables.API;
 
-var apiInstance = new ContactsApi();
+var apiInstance = new SibApiV3Sdk.ContactsApi();
 
-var createContact = new CreateContact(); // CreateContact | Values to create a contact
+var createContact = new SibApiV3Sdk.CreateContact(); // CreateContact | Values to create a contact
 createContact = { 'email' : "ryanzelazny@gmail.com" };
 
 apiInstance.createContact(createContact).then(function(data) {
