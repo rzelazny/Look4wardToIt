a08ec47b6c6f06a888ca676cc9948c5a9a1f1be3
Brianna Hebeler <briannahebeler@gmail.com>
2020-11-09 16:46:34 -0500

    pulled & mereged

---
+++ assets/email.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+  emailjs.init("user_dojrTO2MWFwZrszDi1QQa");
+  
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = Math.random() * 100000 | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm("default_service", "template_jhilbex", "#contact-form")
+          .then(function() {
+              console.log('SUCCESS!');
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+  });
+}
