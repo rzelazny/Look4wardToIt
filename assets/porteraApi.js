0ea67b610411284cb72206b9eaf4f427fc2502da
Alexander Portera <aportera3@gmail.com>
2020-11-10 14:36:36 -0500

    search for similar movies works

--- assets/porteraApi.js
+++ assets/porteraApi.js
@@ -13,7 +13,7 @@ $.ajax({
         // console.log(releaseDate.getTime());
         // console.log(data.results[i].release_date);
         // console.log(today.getTime());
-        if(Date.parse(data.results[i].release_date) <= Date.parse(today)) {
+        if (Date.parse(data.results[i].release_date) <= Date.parse(today)) {
             // console.log("dont show title")
         }
         else {
@@ -31,13 +31,24 @@ $.ajax({
     for (let i = 0; i < data.results.length; i++) {
         // console.log(data.results[i].original_title);
     }
-    })
+})
 
 //ajax call for finding similar movies based on genre and keywords
+var userMovie = "Fight Club";
+var movieId = "";
+
 $.ajax({
     type: "GET",
-    url: "https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=c96c270b94cb65e3e28950111caf5bb7&language=en-US&page=1",
+    url: "https://api.themoviedb.org/3/search/movie?api_key=c96c270b94cb65e3e28950111caf5bb7&language=en-US&query=" + userMovie + "&page=1&include_adult=false",
     dataType: "JSON",
-}).then(function(){
-    
+}).then(function (data) {
+    movieId = data.results[0].id
+    console.log(data.results[0].id)
+    $.ajax({
+        type: "GET",
+        url: "https://api.themoviedb.org/3/movie/" + movieId + "/similar?api_key=c96c270b94cb65e3e28950111caf5bb7&language=en-US&page=1",
+        dataType: "JSON",
+    }).then(function (dataMovie) {
+        console.log(dataMovie);
+    })
 })
