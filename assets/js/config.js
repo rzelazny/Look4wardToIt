0f584bfd2bd684972136aef5a0bf48e77801deea
Brianna Hebeler <briannahebeler@gmail.com>
2020-11-10 17:58:34 -0500

    moved js files into their own folder in assets

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+var configVariables = {
+    emailIDKey: "user_dojrTO2MWFwZrszDi1QQa",
+    NASAKey: "GdEg4FRAzH4iSejZ4CKf8uxEy5RpGsDRREKWJeBT"
+}
+var APIKey = configVariables.NASAKey;
+var configVariables = {
+    emailIDKey: "user_dojrTO2MWFwZrszDi1QQa",
+    NASAKey: "GdEg4FRAzH4iSejZ4CKf8uxEy5RpGsDRREKWJeBT"
+}
