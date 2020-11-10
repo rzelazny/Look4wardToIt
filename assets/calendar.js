3a426349077f429de424046b136d5e4bed8fd565
Alexander Portera <aportera3@gmail.com>
2020-11-10 11:40:50 -0500

    made changes that manoli suggested and started working on now playing movies

---
+++ assets/calendar.js
@@ -0,0 +1,161 @@
+// $(document).ready(function () {
+
+    // id for quote area on the daily page
+    $("#dailyQuote").text("this is where we will be putting the daily quote :)")
+
+    // displays current day on daily view //
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+    $(".monthYearClass").text(moment().format("MMMM YYYY"));
+
+    var today = new Date();
+    var currentMonth = today.getMonth();
+    var currentYear = today.getFullYear();
+    var selectYear = document.getElementById("year");
+    var selectMonth = document.getElementById("month");
+
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    showCalendar(currentMonth, currentYear);
+
+
+    function next() {
+        currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+        currentMonth = (currentMonth + 1) % 12;
+        // currentMonth = (currentMonth + 1); shows the exact same in console
+        // console.log(currentMonth);
+        showCalendar(currentMonth, currentYear);
+    }
+
+    function previous() {
+        currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+        currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+        showCalendar(currentMonth, currentYear);
+    }
+
+    function jump() {
+        currentYear = parseInt(selectYear.value);
+        currentMonth = parseInt(selectMonth.value);
+        showCalendar(currentMonth, currentYear);
+    }
+
+    function showCalendar(month, year) {
+
+        selectYear.value = year;
+        selectMonth.value = month;
+
+        monthBody = document.getElementById("month-body");
+
+        let firstDay = (new Date(year, month)).getDay();
+
+        //clears monthly cal //
+        monthBody.innerHTML = "";
+
+        // creating monthly cal //
+        var date = 1;
+        for (var i = 0; i < 6; i++) {
+            // creates a cal rows //
+            var row = document.createElement("tr");
+
+            //creating individual cells // fill w/ data //
+            for (var j = 0; j < 7; j++) {
+                if (i === 0 && j < firstDay) {
+                    cell = document.createElement("td");
+                    cellText = document.createTextNode("");
+                    cell.appendChild(cellText);
+                    row.appendChild(cell);
+                }
+                else if (date > daysInMonth(month, year)) {
+                    break;
+                }
+
+                else {
+                    cell = document.createElement("td");
+                    cellText = document.createTextNode(date);
+                    input = document.createElement("input");
+                    cell.appendChild(cellText);
+                    row.appendChild(cell);
+                    cell.appendChild(input);
+
+                    if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                        cell.classList.add("bg-warning")
+                    }
+                    date++;
+                }
+            }
+            // append each row into calendar body //
+            monthBody.appendChild(row);
+
+            document.querySelectorAll("#month-body .cell").forEach(cell => {
+                cell.addEventListener("click", event => {
+                    console.log(event.currentTarget);
+                });
+            });
+        }
+    }
+
+    function daysInMonth(iMonth, iYear) {
+        return 32 - new Date(iYear, iMonth, 32).getDate();
+    }
+
+    // document.querySelectorAll("#month-body .input").forEach(cell => {
+    //     cell.addEventListener("click",event => {
+    //         console.log(event.currentTarget);
+    //     });
+    // });
+    ///////////////////////////////////////////////////////////////////////
+    // Getting the containers to display when button for section clicked //
+    ///////////////////////////////////////////////////////////////////////
+
+
+    var monthContainer = document.querySelector("#month-view-container");
+    var weekContainer = document.querySelector("#week-view-container");
+    var dailyContainer = document.querySelector("#daily-view-container");
+    var monthButton = document.querySelector("#month-button");
+    var weekButton = document.querySelector("#week-button");
+    var dailyButton = document.querySelector("#daily-button");
+
+
+    function showMonthView() {
+        if (monthContainer.style.display === "none" && weekContainer.style.display === "block" && dailyContainer.style.display === "none") {
+            monthContainer.style.display = "block";
+            weekContainer.style.display = "none";
+            dailyContainer.style.display = "none";
+        }
+        else if (monthContainer.style.display === "none" && weekContainer.style.display === "none" && dailyContainer.style.display === "block") {
+            monthContainer.style.display = "block";
+            weekContainer.style.display = "none";
+            dailyContainer.style.display = "none";
+        }
+    }
+
+    function showWeekView() {
+        if (monthContainer.style.display === "block" && weekContainer.style.display === "none" && dailyContainer.style.display === "none") {
+            monthContainer.style.display = "none";
+            weekContainer.style.display = "block";
+            dailyContainer.style.display = "none";
+        }
+        else if (monthContainer.style.display === "none" && weekContainer.style.display === "none" && dailyContainer.style.display === "block") {
+            monthContainer.style.display = "none";
+            weekContainer.style.display = "block";
+            dailyContainer.style.display = "none";
+        }
+    }
+
+    function showDailyView() {
+        if (monthContainer.style.display === "block" && weekContainer.style.display === "none" && dailyContainer.style.display === "none") {
+            monthContainer.style.display = "none";
+            weekContainer.style.display = "none";
+            dailyContainer.style.display = "block";
+        }
+        else if (monthContainer.style.display === "none" && weekContainer.style.display === "block" && dailyContainer.style.display === "none") {
+            monthContainer.style.display = "none";
+            weekContainer.style.display = "none";
+            dailyContainer.style.display = "block";
+        }
+    }
+
+    monthButton.addEventListener("click", showMonthView);
+    weekButton.addEventListener("click", showWeekView);
+    dailyButton.addEventListener("click", showDailyView);
+
+// });
--- assets/porteraApi.js
+++ assets/porteraApi.js
@@ -1,18 +1,21 @@
+var today = new Date();
+
 $.ajax({
     type: "GET",
     url: "https://api.themoviedb.org/3/movie/upcoming?api_key=c96c270b94cb65e3e28950111caf5bb7&language=en-US&page=1",
     dataType: "JSON",
-}).then(function (listMovies) {
-    for (let i = 0; i < listMovies.results.length; i++) {
-        console.log(listMovies.results[i].original_title);
-        console.log(listMovies.dates);
-        if (Date.parse(listMovies.results.release_date) < Date.parse(listMovies.dates.minumum)) {
-            console.log("it worked");
-        }
-        else {
-            console.log("it didn't work");
-        }
+}).then(function (data) {
+    for (let i = 0; i < data.results.length; i++) {
+        // console.log(data.results[i].original_title);
     }
 })
 
-
+$.ajax({
+    type: "GET",
+    url: "https://api.themoviedb.org/3/movie/now_playing?api_key=c96c270b94cb65e3e28950111caf5bb7&language=en-US&page=1",
+    dataType: "JSON",
+}).then(function (data) {
+    for (let i = 0; i < data.results.length; i++) {
+        // console.log(data.results[i].original_title);
+    }
+    })
