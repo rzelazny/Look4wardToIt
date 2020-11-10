624fa961121c5796c0158255b54b6dea9939023c
Ryan Zelazny <RyanZelazny@gmail.com>
2020-11-10 11:19:36 -0500

    preventing extra emails during development

--- assets/email.js
+++ assets/email.js
@@ -7,19 +7,21 @@ window.onload = function() {
   var curTime = (moment().format('H'));
   var sendEmail = $("#send_alert").val();
 
+
+
   //send email based on criteria (currently if it's ~8pm for testing)
   if(curTime > 20 && curTime < 21){
     $("#send_mail").value("true");
   }
 
-  if(sendEmail){
+  if(sendEmail === true){
     // send email using EmailJS 
     emailjs.sendForm("default_service", "template_jhilbex", "#email-form")
     .then(function() {
       console.log('Email sent!');
     }, function(error) {
       console.log('Email failed...', error);
-  });
+    });
   }
 
 }
