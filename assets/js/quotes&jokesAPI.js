31599e901bd1b0eda6119aa6f0bb6b4679d7da3e
Brianna Hebeler <briannahebeler@gmail.com>
2020-11-11 14:53:41 -0500

    uncommented all the quotes&jokes api so ryan can take a look

--- assets/js/quotes&jokesAPI.js
+++ assets/js/quotes&jokesAPI.js
@@ -1,20 +1,20 @@
 // Kanye Quote //
 
-// var kanyeRadio = document.querySelector("#kanyeQuote");
+var kanyeRadio = document.querySelector("#kanyeQuote");
 
-// function displayKanye() {
-//     var kanyeURL = "https://api.kanye.rest"
+function displayKanye() {
+    var kanyeURL = "https://api.kanye.rest"
 
-//     $.ajax({
-//         url: kanyeURL,
-//         method: "GET"
-//     }).then(function(data){
-//         console.log("i see kanye");
-//         $("#dailyQuote").text('"' + data.quote + '" - Kanye West');
-//     })
-// }
+    $.ajax({
+        url: kanyeURL,
+        method: "GET"
+    }).then(function(data){
+        console.log("i see kanye");
+        $("#dailyQuote").text('"' + data.quote + '" - Kanye West');
+    })
+}
 
-// kanyeRadio.addEventListener("click", displayKanye());
+kanyeRadio.addEventListener("click", displayKanye());
 
 // Dad Joke //
 
