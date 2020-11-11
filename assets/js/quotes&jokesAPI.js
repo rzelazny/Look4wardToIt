2cea58625449798e66cebe743a27021da17d4717
Brianna Hebeler <briannahebeler@gmail.com>
2020-11-11 14:46:01 -0500

    added dad joke api & it works... i still need to figure out how to use the radio to pick which quote

--- assets/js/quotes&jokesAPI.js
+++ assets/js/quotes&jokesAPI.js
@@ -1,19 +1,36 @@
 // Kanye Quote //
 
-var kanyeRadio = document.querySelector("#kanyeQuote");
+// var kanyeRadio = document.querySelector("#kanyeQuote");
 
-function displayKanye() {
-    var kanyeURL = "https://api.kanye.rest"
+// function displayKanye() {
+//     var kanyeURL = "https://api.kanye.rest"
+
+//     $.ajax({
+//         url: kanyeURL,
+//         method: "GET"
+//     }).then(function(data){
+//         console.log("i see kanye");
+//         $("#dailyQuote").text('"' + data.quote + '" - Kanye West');
+//     })
+// }
+
+// kanyeRadio.addEventListener("click", displayKanye());
+
+// Dad Joke //
+
+var dadJokeRadio = document.querySelector("#dadJoke");
+
+function displayDadJoke() {
+    var dadJokeURL = "https://icanhazdadjoke.com/"
 
     $.ajax({
-        url: kanyeURL,
-        method: "GET"
+        url: dadJokeURL,
+        method: "GET",
+        dataType: "JSON",
     }).then(function(data){
-        console.log("i see kanye");
-        $("#dailyQuote").text('"' + data.quote + '" - Kanye West');
+        // console.log("dad" + data.joke);
+        $("#dailyQuote").text(`"${data.joke}" - Dads of the world`);
     })
 }
 
-kanyeRadio.addEventListener("click", displayKanye());
-
-//
+dadJokeRadio.addEventListener("click", displayDadJoke());
