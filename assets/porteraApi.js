11d7f394a58bbc90a03e133943822733bce53745
Alexander Portera <aportera3@gmail.com>
2020-11-11 10:41:23 -0500

    too the code from script.js and put it in porteraApi.js for reasons

--- assets/porteraApi.js
+++ assets/porteraApi.js
@@ -1,3 +1,22 @@
+//variables for team and their id
+var userTeam = "Buffalo Bills";
+var teamID = ""
+
+//ajax call to find the team id and to search for upcoming events
+$.ajax({
+    type:"GET",
+    url: "https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=" + userTeam,    
+}).then(function(data){
+    teamID= data.teams[0].idTeam
+    console.log(data.teams[0].idTeam);
+    $.ajax({
+        type:"GET",
+        url: "https://www.thesportsdb.com/api/v1/json/1/eventsnext.php?id=" + teamID,
+    }).then(function(teamData){
+        console.log(teamData);
+    })
+})
+
 // variables for the time and release dates
 var today = new Date();
 var releaseDate = new Date();
