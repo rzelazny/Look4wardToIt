2458ef1d961f97f9849b6f71c4f79b02f4a257c2
Brianna Hebeler <briannahebeler@gmail.com>
2020-11-11 11:01:53 -0500

    i can see inside quote js, started kanye api

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,12 +1,10 @@
-// var apiKey = 
+var kanyeURL = "https://api.kanye.rest"
 
-// var quoteURL = "#" + apiKey;
-
-// $.ajax({
-//     url: quoteURL,
-//     method: "GET"
-// }).then(function(data){
-//     var qod = 
-//     //this is where i will create the attachment to
-//     $("dailyQuote").text(qod);
-// })
+$.ajax({
+    url: kanyeURL,
+    method: "GET"
+}).then(function(data){
+    console.log("i can see kanye")
+    //this is where i will create the attachment to
+    // $("dailyQuote").text(qod);
+})
