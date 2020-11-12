9ea272515193b4aa9c0b74f521b0c8065f07a91b
Justin Rhee <jayrhee0514@gmail.com>
2020-11-11 20:00:27 -0500

    commented out modal idea

--- assets/js/NasaAPI.js
+++ assets/js/NasaAPI.js
@@ -7,35 +7,31 @@ $.ajax({
   method: "GET"
 }).then(function(data){
   var imageUrl = data.hdurl;
-  var imageText = data.explanation;
   var spaceImg = $("<img>");
-  var spaceText = $("<p>");
-  console.log(imageText);
   
   spaceImg.attr("src", imageUrl);
-  // spaceImg.attr("alt", "space Image")
-  spaceText.attr("src", imageText);
-  
+  spaceImg.attr("alt", "space Image")
+
   $("#image").prepend(spaceImg); 
-  $("#space-content").prepend(spaceText);
+
 })
 
-// Get the modal
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+// // Get the modal
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("myBtn");
+// var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
