39a46a18b42d06eb24a2d35838d0910030e57598
Justin Rhee <jayrhee0514@gmail.com>
2020-11-12 13:05:29 -0500

    added a hover ability on the page so that I can add the image explanation text

--- assets/js/NasaAPI.js
+++ assets/js/NasaAPI.js
@@ -7,31 +7,13 @@ $.ajax({
   method: "GET"
 }).then(function(data){
   var imageUrl = data.url;
+  var imageText = data.explanation
   var spaceImg = $("<img>");
-  
+  var spaceText = $("<p>");
+
   spaceImg.attr("src", imageUrl);
   spaceImg.attr("alt", "space Image")
 
-  $("#image").prepend(spaceImg); 
+  $(".image").prepend(spaceImg); 
 
 })
-
-// // Get the modal
-// var modal = document.getElementById("myModal");
-// var btn = document.getElementById("myBtn");
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks the button, open the modal 
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
