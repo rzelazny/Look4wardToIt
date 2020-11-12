e282b535df232caed99cff709ca8d06a64d5d3f7
Brianna Hebeler <briannahebeler@gmail.com>
2020-11-12 14:58:41 -0500

    connected calapi js and index & got the api all set up

--- assets/js/calAPI.js
+++ assets/js/calAPI.js
@@ -1,11 +1,11 @@
 // get holidays //
-
-var holidayURL = "https://calendarific.com/api/v2/holidays"
+var calApiKey = "712601737a8c6a737a00b16a646c175ecbbbd2df";
+var holidayURL = "https://calendarific.com/api/v2/holidays?api_key=" + calApiKey + "&country=US&year=2020";
 
 $.ajax({
     url: holidayURL,
     method: "GET",
     dataType: "JSON",
 }).then(function(data){
-    console.log("see me here: " + data)
+    console.log("see me here: " + data);
 })
