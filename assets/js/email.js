011996337707a412107891c1b8bc8b4838354f12
Ryan Zelazny <RyanZelazny@gmail.com>
2020-11-12 11:16:25 -0500

    updated to use .send instead of .sendForm

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,24 +1,47 @@
 window.onload = function() {
 
-  // Initialize email client with API key
-  emailjs.init(configVariables.emailIDKey);
-  
-  // Get time to see if we need to send emails yet
-  var curTime = (moment().format('H'));
-  var sendEmail = $("#send_alert").val();
-
-  //send email based on criteria (currently if it's ~8pm for testing)
-  if(curTime > 20 && curTime < 21){
-    $("#send_mail").value("true");
+  //object stores the parameters used to populate email template
+  var emailParams = {
+    user_name: "",
+    upcoming_event: "",
+    message: ""
   }
 
-//   if(sendEmail){
-//     // send email using EmailJS 
-//     // emailjs.sendForm("default_service", "template_jhilbex", "#email-form")
-//     .then(function() {
-//       console.log('Email sent!');
-//     }, function(error) {
-//       console.log('Email failed...', error);
-//   });
-//   }
+  // Initialize email client with API key
+  emailjs.init(configVariables.emailIDKey);
+
+  //Check if events need a reminder set when calendar is opened
+  eventReminder();
+  
+  // Get time to see if we need to send emails yet
+  //var curTime = (moment().format('H'));
+  
+
+  //function sends an email if there is an event tomorrow that hasn't had a reminder sent yet
+  function eventReminder(){
+    
+    emailParams.message = "This is a test message";
+    emailParams.user_name = "Ryan";
+    emailParams.upcoming_event = "Pinewood Derby";
+
+    if(events.reminderSent === false){
+      //sendEmail(emailParams);
+    }
+
+  }
+
+  //var sendEmail = $("#send_alert").val();
+
+
+
+
+  //function sends an email using EmailJS
+  function sendEmail (emailParams){
+      emailjs.send("default_service", "template_jhilbex", emailParams)
+      .then(function() {
+        console.log('Email sent!');
+      }, function(error) {
+        console.log('Email failed...', error);
+    });
+  }
 }
